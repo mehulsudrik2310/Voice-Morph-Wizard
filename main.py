959b7829993458b994d68d9a99d8f3781aee9056
Mehul Sudrik <mdsudrik@gmail.com>
2023-12-06 20:15:48 -0500

    Added proper comments to the functions

--- main.py
+++ main.py
@@ -15,55 +15,81 @@ paused_position = 0  # Track the paused position
 update_bar_thread_running = False  # New flag to control the thread
 
 def play_audio():
+    # Access global variables that will be modified in this function
     global selected_file_path, play_obj, is_playing, paused_position, update_bar_thread_running
+
+    # Check if a file path has been selected
     if selected_file_path:
         # Signal to stop the current play bar update thread
         update_bar_thread_running = False
 
+        # Stop any currently playing audio
         if play_obj:
             play_obj.stop()
 
-        # Wait briefly to ensure the thread stops
+        # Wait briefly to ensure the thread stops before starting a new playback
         time.sleep(0.1)
 
+        # Reset the play bar to the start position
         play_bar.set(0)
+
+        # Load the audio file and prepare it for playback
         audio = AudioSegment.from_file(selected_file_path)
         play_obj = sa.play_buffer(audio.raw_data, num_channels=audio.channels, bytes_per_sample=audio.sample_width, sample_rate=audio.frame_rate)
+
+        # Set flags to indicate audio is now playing and reset the paused position
         is_playing = True
         paused_position = 0
 
         # Start a new thread to update the play bar
         update_bar_thread_running = True
         threading.Thread(target=update_play_bar, daemon=True).start()
-        # Update button states
+
+        # Update the state of toggle button to show "Pause" and enable it
         toggle_button.config(text="Pause", state=tk.NORMAL)
 
 def toggle_pause_continue():
+    # Access global variables that will be modified in this function
     global play_obj, is_playing, paused_position, update_bar_thread_running
+
+    # Check if the audio is currently playing
     if is_playing:
         # Pause the audio
         play_obj.stop()
+        # Set flags to indicate audio is now paused
         is_playing = False
+        # Save the current position of the audio for resuming later
         paused_position = int(play_bar.get() * 1000)  # Convert to milliseconds
+        # Update the toggle button to show "Continue"
         toggle_button.config(text="Continue")
     else:
-        # Continue playing from paused position
+        # Resume playing audio from the paused position
         if selected_file_path:
             audio = AudioSegment.from_file(selected_file_path)
             audio = audio[paused_position:]
             play_obj = sa.play_buffer(audio.raw_data, num_channels=audio.channels, bytes_per_sample=audio.sample_width, sample_rate=audio.frame_rate)
             is_playing = True
             threading.Thread(target=update_play_bar, daemon=True).start()
+            # Update the toggle button to show "Pause"
             toggle_button.config(text="Pause")
 
 def update_play_bar():
+    # Access global variables used in this function
     global is_playing, play_obj, paused_position, update_bar_thread_running
+
+    # Record the start time for calculating the current position
     start_time = time.time()
+
+    # Continuously update the play bar while the audio is playing
     while is_playing and play_obj.is_playing() and update_bar_thread_running:
+        # Calculate the current position in the audio file
         current_pos = (time.time() - start_time) + paused_position / 1000  # in seconds
+        # Update the play bar with the current position
         play_bar.set(current_pos)
+        # Break the loop if the end of the audio is reached
         if current_pos >= audio_length:
             break
+        # Sleep briefly to avoid overloading the CPU
         time.sleep(0.1)
 
 def on_microphone_click():
@@ -72,12 +98,20 @@ def on_microphone_click():
 
 def on_audio_click(audio_number):
     print(f"Audio {audio_number} button clicked")
+
+    # Access the global variable that tracks the currently selected audio button
     global selected_audio_button
+
+    # Deselect the previously selected button if it's not the output button
     if selected_audio_button and selected_audio_button != output_button:
         toggle_button_color(selected_audio_button)
+
+    # If the same button is clicked again, set selected_audio_button to None,
+    # effectively deselecting it
     if selected_audio_button == audio_buttons[audio_number]:
         selected_audio_button = None
     else:
+        # Select the newly clicked button and update its color
         selected_audio_button = audio_buttons[audio_number]
         toggle_button_color(selected_audio_button)
 
@@ -93,33 +127,43 @@ def toggle_button_color(button):
         button.configure(background=original_color)
 
 def on_upload_audio():
+    # Access the global variables that will be modified in this function
     global selected_file_path, audio_length, paused_position, is_playing, play_obj
+
+    # Open a file dialog for the user to select an audio file
     file_path = filedialog.askopenfilename(title="Upload Audio File", filetypes=[("Audio Files", "*.mp3;*.wav")])
+    
+    # Check if a file was selected (file_path is not empty)
     if file_path:
-        # Stop currently playing audio, if any
+        # If there's an audio playing, stop it to load the new file
         if play_obj:
             play_obj.stop()
 
-        # Reset playback variables
+        # Reset playback variables to ensure clean state for new audio
         is_playing = False
         paused_position = 0
         play_bar.set(0)
 
-        # Update UI elements
+        # Update the UI with the selected file's name and enable the Convert button
         print(f"Selected file: {file_path}")
         shortened_file_name = shorten_file_name(file_path, max_chars=15)
         selected_file_label.config(text=f"Selected File: {shortened_file_name}")
         convert_button.config(state=tk.NORMAL)  # Enable the Convert button
+
+        # Update the global variable for the selected file path
         selected_file_path = file_path
 
-        # Load the audio file and set the play bar's range
+        # Load the audio file to determine its length
         audio = AudioSegment.from_file(file_path)
         audio_length = len(audio) / 1000  # Length of audio in seconds
+
+        # Configure the play bar's range to match the length of the audio
         play_bar.config(to=audio_length)
 
-        # Update button states
+        # Reset the state of play and toggle buttons for the new audio
         play_button.config(state=tk.NORMAL)
         toggle_button.config(state=tk.DISABLED, text="Pause")
+
 
 def on_upload_effects():
     effects_path = filedialog.askopenfilename(title="Upload Effects File", filetypes=[("Audio Files", "*.mp3;*.wav")])
